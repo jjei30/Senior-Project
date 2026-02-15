65e0995667e98a67c1ae5141af0a890507a42c6d
Reinu <reinusees@gmail.com>
2026-02-15 11:01:11 +0200

    adding an iscollapsed boolean method to ensure that every cell has been reduced to a tile

--- turnBasedGame/src/Map.java
+++ turnBasedGame/src/Map.java
@@ -7,7 +7,7 @@ public class Map{
     }
 
     
-    private int size;
+    private int size; 
     private Set<Tile>[][] mapGrid;
     private Random random = new Random();
 
@@ -15,11 +15,28 @@ public class Map{
         this.size = size;
         mapGrid = new HashSet[size][size];
 
+        //initializing all possible tiles
         for(int x=0; x < size; x++){
             for(int y=0; y < size; y++){
                 mapGrid[x][y] = new HashSet<>(Arrays.asList(Tile.values()));
             }
         }
+        
+    }
+
+    //check for every cell if it has been reduced to a tile
+    private boolean isCollapsed(){
+        for(int x=0; x < size; x++){
+            for(int y=0; y < size; y++){
+                if(mapGrid[x][y].size() > 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void Generate(){
         
     }
 }
