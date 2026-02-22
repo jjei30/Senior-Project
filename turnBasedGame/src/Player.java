3ac37ae5d72c922fb254ffcf98ddc639e77f12f5
Reinu <reinusees@gmail.com>
2026-02-23 00:53:06 +0200

    initializing player class

--- turnBasedGame/src/Player.java
+++ turnBasedGame/src/Player.java
@@ -1,3 +1,18 @@
 public class Player {
-    
+    private int x;
+    private int y;
+
+    //for starting position
+    public Player(int startingX, int startingY){
+        this.x = startingX;
+        this.y = startingY;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 }
