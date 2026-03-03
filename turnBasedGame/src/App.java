64b6d4e2e4332cb5fb38f2d545f395e590f09583
Reinu <reinusees@gmail.com>
2026-03-03 22:59:34 +0200

    Removed the functionalities from the App.java

--- turnBasedGame/src/App.java
+++ turnBasedGame/src/App.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        boolean active = true;
-        String input = scanner.nextLine();
-        while(active){
-            Map map = new Map(10); //10x10
-            map.generateMap();
-            map.mapPrint();
-        }
+        Map map = new Map(10); //10x10
+        map.generateMap();
+        map.mapPrint();
         
     }
 }
