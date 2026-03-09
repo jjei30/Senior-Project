a4124f556b3d8ea5c93dcf72952071fe16d573d4
Reinu <reinusees@gmail.com>
2026-03-09 13:25:11 +0200

    updating documentation links and working on the findpath

--- turnBasedGame/src/Pathfinder.java
+++ turnBasedGame/src/Pathfinder.java
@@ -17,4 +17,24 @@ public class Pathfinder {
             f = g + h;
         }
     }
+    //start x,y would be the enemy's pos while goal x,y is where the player is
+    public static List<Node> findPath(Map map, int startX,int startY, int goalX, int goalY){
+
+        PriorityQueue<Node> openSet = new PriorityQueue<>(Comparator.comparingInt(n->n.f));
+
+        List<Node> closedSet = new ArrayList<>();
+
+        Node start = new Node(startX, startY);
+
+        openSet.add(start);
+
+        while(!openSet.isEmpty()){
+            //checking the lowest f(n)
+            Node current = openSet.poll();
+
+            if(current.x == goalX && current.y = goalY){
+                return; //placeholder
+            }
+        }
+    }
 }
